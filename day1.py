296082971f648c85afc453ef4690ffa780aa1dd5
ryotokuro <ryotokuro@gmail.com>
2021-01-04 00:15:52 +1100

    had some issues with readline() because i didn't sanitise the input and strip it initially before casting but now this solution works :)

--- day1.py
+++ day1.py
@@ -1,10 +1,24 @@
 # https://adventofcode.com/2020/day/1
 
-report = [1721, 979, 366, 299, 675, 1456]
-diff = [2000 - i for i in report]
-print(diff)
+report = []  # create list to store input report
 
-for i in diff:
-    if i in report:
-        return 
-    
+f = open("input1.txt", "r")  # read input from file
+
+while True:
+    line = f.readline()
+    # check if line is null
+    if not line:
+        break
+
+    n = int(line.strip())
+    report.append(n)  # populate list
+
+diff = {}
+magic = 2020
+for expense in report:
+    diff[expense] = magic - expense  # calculate difference from desired number
+
+for key, val in diff.items():
+    if val in report:
+        print(key*val)  # print result
+        break  # return once we find it
