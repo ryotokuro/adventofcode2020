928523b4140f7ce69f4b547a1eb7fed0647d8e25
ryotokuro <ryotokuro@gmail.com>
2021-01-03 23:56:19 +1100

    trying an O(n) solution

--- day1.py
+++ day1.py
@@ -1,1 +1,10 @@
 # https://adventofcode.com/2020/day/1
+
+report = [1721, 979, 366, 299, 675, 1456]
+diff = [2000 - i for i in report]
+print(diff)
+
+for i in diff:
+    if i in report:
+        return 
+    
